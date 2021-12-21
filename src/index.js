e31a211731d142a430b096d120f1c6d7e1d2d950
Ibrahim Ibrahim <folahanmi0001@gmail.com>
2021-12-21 15:46:10 +0100

    home header

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import App from "../src/pages/App/App";
-import Work from "../src/pages/Work/Work";
+import App from "../src/pages/app/app";
+// import Work from "./pages/work/work";
 // import SiteRoutes from "./routes.js";
 
 ReactDOM.render(
@@ -12,7 +12,7 @@ ReactDOM.render(
     <Router>
       <Routes>
         <Route exact path="/" element={<App />}></Route>
-        <Route path="/work" element={<Work />}></Route>
+        {/* <Route path="/work" element={<Work />}></Route> */}
       </Routes>
     </Router>
   </React.StrictMode>,
